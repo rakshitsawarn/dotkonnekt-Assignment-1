4d64edf252ab5df1a1ad403e9cc00c9ccd616e6d
rakshitsawarn <rakshitsawarn@gmail.com>
2025-10-19 16:34:37 +0530

    Readablity and  understanding are increased and code is now more clean and maintainable

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,12 +2,18 @@
 
 const express = require('express');
 
-// Router Object will help in defining multiple routes
+// Router Object will help in defining multiple
 const router = express.Router();
+
+// Importing these functions from taskController.js present inside controllers folder
 const { scheduleTask, getTasks, cancelTask } = require('../controllers/taskController');
 
+
+// Defining routes, when someone will send request Express will call these functions
 router.post('/schedule', scheduleTask);
 router.get('/tasks', getTasks);
 router.delete('/cancel/:id', cancelTask);
 
+
+// Exports the router object so that other modules can use it
 module.exports = router;
